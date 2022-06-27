ab3f9764044a83f9c447aab7b90238c79238f1ec
robrumput <108051558+robrumput@users.noreply.github.com>
2022-06-27 19:57:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+var placeholderPW = document.getElementById("placeholderPW")
+
+function generatepasswords(){
+    var passwordlenght = 9
+    var password = ""
+    
+    for (var i=0; i<= passwordlenght; i++){
+        var randomPW = Math.floor(Math.random() * characters.length)
+        password += characters[randomPW]   
+    }
+    document.getElementById("placeholderPW").value = password
+    
+}
+
+function copypasswords() {
+  var copyText = document.getElementById("placeholderPW");
+  copyText.select();
+  document.execCommand("copy"); 
+  document.getElementById("placeholderPW").value = ""
+}
